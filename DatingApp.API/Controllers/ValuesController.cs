e12f1d49ab2918ce658d699651ee05af778f2f88
husamnemer <dev.husam@gmail.com>
2019-05-14 23:31:16 +0300

    first commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _Dbcontext ;
+
+        public ValuesController(DataContext dbcontext)
+        {
+            _Dbcontext = dbcontext;
+
+        }
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+          var data = _Dbcontext.value.ToList();
+           return Ok(data);
+        }
+
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var values=_Dbcontext.value.FirstOrDefault(x=>x.ID==id);
+            return Ok(values);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DatingApp.API/Model/Values.cs
@@ -0,0 +1,10 @@
+namespace DatingApp.API.Model
+{
+    public class Values
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
